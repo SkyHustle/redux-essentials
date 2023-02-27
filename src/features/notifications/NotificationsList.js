b1337b19274c384532ee221f0f39d7b8b321bda4
Dmitry Sky <dmitry.sky@icloud.com>
2023-02-27 13:00:47 -0700

    Notifications Page

---
+++ src/features/notifications/NotificationsList.js
@@ -0,0 +1,33 @@
+import { useSelector } from "react-redux"
+import { formatDistanceToNow, parseISO } from "date-fns"
+
+export const NotificationsList = () => {
+  const notifications = useSelector((state) => state.notifications)
+  const users = useSelector((state) => state.users)
+
+  const renderedNotifications = notifications.map((notification) => {
+    const date = parseISO(notification.date)
+    const timeAgo = formatDistanceToNow(date)
+    const user = users.find((user) => user.id === notification.user) || {
+      name: "Unknown User",
+    }
+
+    return (
+      <div key={notification.id} className="notification">
+        <div>
+          <b>{user.name}</b> {notification.message}
+        </div>
+        <div title={notification.date}>
+          <i>{timeAgo} ago</i>
+        </div>
+      </div>
+    )
+  })
+
+  return (
+    <section className="notificationsList">
+      <h2>Notifications</h2>
+      {renderedNotifications}
+    </section>
+  )
+}
