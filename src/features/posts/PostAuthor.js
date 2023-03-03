951676f4b175bed6aa523080ca241a67966e8496
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-02 18:24:21 -0700

    Link to post author page from inside post

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -1,9 +1,13 @@
-import React from "react"
 import { useSelector } from "react-redux"
 import { selectUserById } from "../users/usersSlice"
+import { Link } from "react-router-dom"
 
 export const PostAuthor = ({ userId }) => {
   const author = useSelector((state) => selectUserById(state, userId))
 
-  return <span>by {author ? author.name : "Unknown author"}</span>
+  return (
+    <Link to={`/users/${userId}`}>
+      by {author ? author.name : "Unknown author"}
+    </Link>
+  )
 }
