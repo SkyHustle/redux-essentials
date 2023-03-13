63a71c69df85e3c5abcef2aeca5b9610df09a80f
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-13 11:33:54 -0600

    Sort and Memoize posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,21 +1,31 @@
+import { useMemo } from "react"
 import { Spinner } from "../../components/Spinner"
 import { PostExcerpt } from "./PostExcerpt"
 import { useGetPostsQuery } from "../api/apiSlice"
 
 export const PostsList = () => {
   const {
-    data: posts,
+    data: posts = [],
     isLoading,
     isSuccess,
     isError,
     error,
   } = useGetPostsQuery()
 
+  const sortedPosts = useMemo(() => {
+    const sortedPosts = posts.slice()
+    // Sort posts in descending chronological order
+    sortedPosts.sort((a, b) => b.date.localeCompare(a.date))
+    return sortedPosts
+  }, [posts])
+
   let content
   if (isLoading) {
     content = <Spinner text="Loading..." />
   } else if (isSuccess) {
-    content = posts.map((post) => <PostExcerpt key={post.id} post={post} />)
+    content = sortedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
   } else if (isError) {
     content = <div>{error.toString()}</div>
   }
