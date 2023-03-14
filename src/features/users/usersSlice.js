0489033fe21a070a636eb693fe91729f5b678b61
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-14 13:41:31 -0600

    Transform response, Normalize data in a thunkee way

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,63 +1,32 @@
-import {
-  // createSlice,
-  createSelector,
-  // createAsyncThunk,
-  // createEntityAdapter,
-} from "@reduxjs/toolkit"
-// import { client } from "../../api/client"
-
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit"
 import { apiSlice } from "../api/apiSlice"
 
-// export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
-//   const response = await client.get("/fakeApi/users")
-//   return response.data
-// })
+const usersAdapter = createEntityAdapter()
+const initialState = usersAdapter.getInitialState()
 
-// const usersAdapter = createEntityAdapter()
-// const initialState = usersAdapter.getInitialState()
-
-// same object as apiSlice, important when using TypeScript
 export const extendedApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: () => "/users",
+      transformResponse: (responseData) => {
+        return usersAdapter.setAll(initialState, responseData)
+      },
     }),
   }),
 })
 
 export const { useGetUsersQuery } = extendedApiSlice
 
+// Calling `someEndpoint.select(someArg)` generates a new selector that will return
+// the query result object for a query with those parameters.
+// To generate a selector for a specific query argument, call `select(theQueryArg)`.
+// In this case, the users query has no params, so we don't pass anything to select()
 export const selectUsersResult = extendedApiSlice.endpoints.getUsers.select()
 
-const emptyUsers = []
-
-export const selectAllUsers = createSelector(
+const selectUsersData = createSelector(
   selectUsersResult,
-  (usersResult) => usersResult?.data ?? emptyUsers
+  (usersResult) => usersResult.data
 )
 
-export const selectUserById = createSelector(
-  selectAllUsers,
-  (state, userId) => userId,
-  (users, userId) => users.find((user) => user.id === userId)
-)
-
-// const usersSlice = createSlice({
-//   name: "users",
-//   initialState,
-//   reducers: {},
-//   extraReducers(builder) {
-//     builder.addCase(fetchUsers.fulfilled, (state, action) => {
-//       usersAdapter.setAll(state, action.payload)
-//     })
-//   },
-// })
-//
-// export default usersSlice.reducer
-//
-// export const {
-//   selectAll: selectAllUsers,
-//   selectById: selectUserById,
-//   selectIds: selectUserIds,
-//   // Pass in a selector that returns the users slice of state
-// } = usersAdapter.getSelectors((state) => state.users)
+export const { selectAll: selectAllUsers, selectById: selectUserById } =
+  usersAdapter.getSelectors((state) => selectUsersData(state) ?? initialState)
