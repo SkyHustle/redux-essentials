f243bfc84a152efd429b39ba043a1a7201a47f91
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-15 13:30:25 -0600

    Streaming cache updates for notifications from the server

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -1,53 +1,70 @@
-import {
-  createSlice,
-  createAsyncThunk,
-  createEntityAdapter,
-} from "@reduxjs/toolkit"
-import { client } from "../../api/client"
+import { createSelector } from "@reduxjs/toolkit"
+import { forceGenerateNotifications } from "../../api/server"
+import { apiSlice } from "../api/apiSlice"
 
-export const fetchNotifications = createAsyncThunk(
-  "notifications/fetchNotifications",
-  async (_, { getState }) => {
-    const [latestNotification] = selectAllNotifications(getState())
-    const latestTimestamp = latestNotification ? latestNotification.date : ""
-    const response = await client.get(
-      `/fakeApi/notifications?since=${latestTimestamp}`
-    )
-    return response.data
-  }
+export const extendedApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getNotifications: builder.query({
+      query: () => "/notifications",
+      async onCacheEntryAdded(
+        arg,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) {
+        // create a websocket connection when the cache subscription starts
+        const ws = new WebSocket("ws://localhost")
+        try {
+          // wait for the initial query to resolve before proceeding
+          await cacheDataLoaded
+
+          // when data is received from the socket connection to the server,
+          // update our query result with the received message
+          const listener = (event) => {
+            const message = JSON.parse(event.data)
+            switch (message.type) {
+              case "notifications": {
+                updateCachedData((draft) => {
+                  // Insert all received notifications from the websocket
+                  // into the existing RTKQ cache array
+                  draft.push(...message.payload)
+                  draft.sort((a, b) => b.date.localeCompare(a.date))
+                })
+                break
+              }
+              default:
+                break
+            }
+          }
+
+          ws.addEventListener("message", listener)
+        } catch {
+          // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`,
+          // in which case `cacheDataLoaded` will throw
+        }
+        // cacheEntryRemoved will resolve when the cache subscription is no longer active
+        await cacheEntryRemoved
+        // perform cleanup steps once the `cacheEntryRemoved` promise resolves
+        ws.close()
+      },
+    }),
+  }),
+})
+
+export const { useGetNotificationsQuery } = extendedApi
+
+const emptyNotifications = []
+
+export const selectNotificationsResult =
+  extendedApi.endpoints.getNotifications.select()
+
+const selectNotificationsData = createSelector(
+  selectNotificationsResult,
+  (notificationsResult) => notificationsResult.data ?? emptyNotifications
 )
 
-const notificationsAdapter = createEntityAdapter({
-  // Sort with newest first
-  sortComparer: (a, b) => b.date.localeCompare(a.date),
-})
-const initialState = notificationsAdapter.getInitialState()
-
-const notificationsSlice = createSlice({
-  name: "notifications",
-  initialState,
-  reducers: {
-    allNotificationsRead(state, action) {
-      Object.values(state.entities).forEach((notification) => {
-        notification.read = true
-      })
-    },
-  },
-  extraReducers(builder) {
-    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
-      notificationsAdapter.upsertMany(state, action.payload)
-
-      Object.values(state.entities).forEach((notification) => {
-        // Any notifications we've read are no longer new
-        notification.isNew = !notification.read
-      })
-    })
-  },
-})
-
-export const { allNotificationsRead } = notificationsSlice.actions
-export default notificationsSlice.reducer
-
-// Export the customized selectors for this adapter using `getSelectors`
-export const { selectAll: selectAllNotifications } =
-  notificationsAdapter.getSelectors((state) => state.notifications)
+export const fetchNotificationsWebsocket = () => (dispatch, getState) => {
+  const allNotifications = selectNotificationsData(getState())
+  const [latestNotification] = allNotifications
+  const latestTimestamp = latestNotification?.date ?? ""
+  // Hardcode a call to the mock server to simulate a server push scenario over websockets
+  forceGenerateNotifications(latestTimestamp)
+}
